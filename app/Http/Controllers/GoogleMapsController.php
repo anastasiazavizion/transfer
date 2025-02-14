74ab7d431dbacb6f2c649e9c998d1e4d34985a38
Anastasia Zavizion <a.zavizion.a@gmail.com>
2025-02-14 21:41:35 +0100

    Show distance and duration info

---
+++ app/Http/Controllers/GoogleMapsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GoogleMaps\DistanceDurationRequest;
+use App\Services\GoogleMapsService;
+use Illuminate\Http\Request;
+
+class GoogleMapsController extends Controller
+{
+    public function __construct(public GoogleMapsService $googleMapsService)
+    {
+    }
+
+    public function distanceDuration(DistanceDurationRequest $request): \Illuminate\Http\JsonResponse
+    {
+        return $this->googleMapsService->getDistanceDuration($request->validated());
+    }
+}
--- resources/js/Pages/Destination.vue
+++ resources/js/Pages/Destination.vue
@@ -9,22 +9,41 @@ const data = computed(() => {
   return store.getters['getFormData']
 })
 
-const mapSrc = ref(""); // Ref to store the map image URL
+const mapSrc = ref("");
+const distance = ref("");
+const duration = ref("");
 
 onMounted(async () => {
+
   if (Object.keys(data.value).length > 0) {
-    console.log(data.value);
+
     mapSrc.value = "https://maps.googleapis.com/maps/api/staticmap?size=640x300&markers=place:" + data.value.address_from + "&markers=place:" + data.value.address_to + "&path=color:0x0000ff|weight:5|" + data.value.address_from + "|" + data.value.address_to + "&key=" + import.meta.env.VITE_GOOGLE_API_KEY;
+    const response =  await axios.get(route('distanceDuration'), {params:data.value});
+    if(response.data){
+      distance.value = response.data.distance;
+      duration.value = response.data.duration;
+    }
+
   }
 })
 
 </script>
 
 <template>
-  <div class="row">
+  <div class="row text-black">
     <div class="col-md-12 col-lg-12 d-flex justify-content-center">
       <img class="mx-auto" :src="mapSrc" alt="Map Image" v-if="mapSrc"/>
     </div>
+
+    <div v-if="distance" class="col-md-12 col-lg-12">
+      <div>Distance = {{distance}}</div>
+    </div>
+
+    <div v-if="duration" class="col-md-12 col-lg-12">
+      <div>Distance = {{duration}}</div>
+    </div>
+
+
   </div>
 </template>
 
